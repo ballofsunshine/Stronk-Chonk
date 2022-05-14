1d8310fae97db7b2dec9d65f72309cf3cf03bc91
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-05-14 19:45:09 -0400

    Initial Commit

---
+++ Stronk-Chonk-iOS/Stronk-Chonk-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stronk-Chonk-iOS
+//
+//  Created by Niels Mainville on 2022-05-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
